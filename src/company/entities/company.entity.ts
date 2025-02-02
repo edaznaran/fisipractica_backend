8717d3b2949a89b66292879bdb62befae6ed7cde
Gerson Aznarán <gersonec12@gmail.com>
2025-02-02 00:07:47 -0500

    Define company entity

--- src/company/entities/company.entity.ts
+++ src/company/entities/company.entity.ts
@@ -1,1 +1,34 @@
-export class Company {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Company {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  address: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  website: string;
+
+  @Column()
+  location: string;
+
+  @CreateDateColumn()
+  created_date: Date;
+
+  @UpdateDateColumn()
+  updated_date: Date;
+}
