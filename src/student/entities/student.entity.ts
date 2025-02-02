44f432407828d5bba1c22758db1336ce63486fd0
Gerson Aznarán <gersonec12@gmail.com>
2025-02-02 00:04:41 -0500

    Define student entity

--- src/student/entities/student.entity.ts
+++ src/student/entities/student.entity.ts
@@ -1,1 +1,40 @@
-export class Student {}
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class Student {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  code: string;
+
+  @Column()
+  cv_url: string;
+
+  @Column()
+  institution: string;
+
+  @Column()
+  education_start_date: Date;
+
+  @Column()
+  education_end_date: Date;
+
+  @Column()
+  studying: boolean;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @CreateDateColumn()
+  created_date: Date;
+
+  @UpdateDateColumn()
+  updated_date: Date;
+}
