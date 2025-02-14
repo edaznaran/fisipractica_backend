43000958ae795ca4517b5e4977cc6145343b1d32
Gerson Aznarán <gersonec12@gmail.com>
2025-02-13 19:41:24 -0500

    Add company CRUD

--- src/company/company.controller.ts
+++ src/company/company.controller.ts
@@ -1,33 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
+import { ApiBearerAuth, ApiOperation } from '@nestjs/swagger';
 import { CompanyService } from './company.service';
 import { CreateCompanyDto } from './dto/create-company.dto';
 import { UpdateCompanyDto } from './dto/update-company.dto';
 
+@ApiBearerAuth('JWT-auth')
 @Controller('company')
 export class CompanyController {
   constructor(private readonly companyService: CompanyService) {}
 
   @Post()
+  @ApiOperation({ summary: 'Crear empresa' })
   create(@Body() createCompanyDto: CreateCompanyDto) {
     return this.companyService.create(createCompanyDto);
   }
 
   @Get()
+  @ApiOperation({ summary: 'Listar todas las empresas' })
   findAll() {
     return this.companyService.findAll();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Buscar empresa por ID' })
   findOne(@Param('id') id: string) {
     return this.companyService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOperation({ summary: 'Actualizar empresa por ID' })
   update(@Param('id') id: string, @Body() updateCompanyDto: UpdateCompanyDto) {
     return this.companyService.update(+id, updateCompanyDto);
   }
 
   @Delete(':id')
+  @ApiOperation({ summary: 'Eliminar empresa por ID' })
   remove(@Param('id') id: string) {
     return this.companyService.remove(+id);
   }
--- src/company/dto/create-company.dto.ts
+++ src/company/dto/create-company.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCompanyDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString } from 'class-validator';
+
+export class CreateCompanyDto {
+  @ApiProperty()
+  @IsString()
+  name: string;
+
+  @ApiProperty()
+  @IsString()
+  address: string;
+
+  @ApiProperty()
+  @IsString()
+  description: string;
+
+  @ApiProperty()
+  @IsString()
+  website: string;
+
+  @ApiProperty()
+  @IsString()
+  location: string;
+}
