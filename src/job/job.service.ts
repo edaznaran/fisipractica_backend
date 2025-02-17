b7f869db2c6ee0696f0dd433ba78777b65ffd517
LKA102 <stefano.hernandez@unmsm.edu.pe>
2025-02-17 00:36:18 -0500

    Implement job controller

--- src/job/job.service.ts
+++ src/job/job.service.ts
@@ -1,26 +1,102 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  HttpException,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateJobDto } from './dto/create-job.dto';
 import { UpdateJobDto } from './dto/update-job.dto';
+import { Job } from './entities/job.entity';
 
 @Injectable()
 export class JobService {
-  create(createJobDto: CreateJobDto) {
-    return 'This action adds a new job';
+  constructor(
+    @InjectRepository(Job)
+    private readonly jobRepository: Repository<Job>,
+  ){};
+
+  async create(createJobDto: CreateJobDto) {
+    try{
+      const job = this.jobRepository.create(createJobDto);
+      return await this.jobRepository.save(job);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al crear el trabajo');
+    }
   }
 
-  findAll() {
-    return `This action returns all job`;
+  async findAll() {
+    try{
+      const jobs = await this.jobRepository.find();
+      return jobs;
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al listar los trabajos');
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} job`;
+  async findOne(id: number) {
+    try{
+      const job = await this.jobRepository.findOne({
+        where: { id },
+        relations: ['company', 'userProfile'],
+      });
+      if (!job) {
+        throw new NotFoundException(`Trabajo con id ${id} no encontrado`);
+      }
+      return job;
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al buscar el trabajo');
+    }
   }
 
-  update(id: number, updateJobDto: UpdateJobDto) {
-    return `This action updates a #${id} job`;
+  async update(id: number, updateJobDto: UpdateJobDto) {
+    try{
+      const job = await this.jobRepository.findOneBy({id});
+      if (!job) {
+        throw new NotFoundException(`Trabajo con id ${id} no encontrado`);
+      }
+      Object.assign(job, updateJobDto);
+      return await this.jobRepository.save(job);
+    } catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al actualizar el trabajo');
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} job`;
+  async remove(id: number) {
+    try{
+      const job = await this.jobRepository.findOne({
+        where: { id },
+        relations: ['company', 'userProfile'],
+      });
+      if (!job) {
+        throw new NotFoundException(`Trabajo con id ${id} no encontrado`);
+      }
+      return await this.jobRepository.remove(job);
+    }
+    catch (error) {
+      console.error(error);
+      if (error instanceof HttpException) {
+        throw error;
+      }
+      throw new InternalServerErrorException('Error al eliminar el trabajo');
+    }
   }
 }
