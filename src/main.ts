e85ae04bed51b6f66ce722a977fa61f6dac38e2d
Gerson Aznarán <gersonec12@gmail.com>
2025-07-06 17:50:50 -0500

    fix(swagger): corregir la ruta de customSwaggerUiPath en la configuración de Swagger

--- src/main.ts
+++ src/main.ts
@@ -33,7 +33,7 @@ async function bootstrap() {
     swaggerOptions: {
       persistAuthorization: true,
     },
-    customSwaggerUiPath: './node_modules/swagger-ui-dist',
+    customSwaggerUiPath: 'node_modules/swagger-ui-dist',
   });
 
   await app.listen(process.env.PORT ?? 3000);
