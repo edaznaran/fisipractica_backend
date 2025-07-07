5f5deaba8d7b370043632cf49d8f98a35422a60a
Gerson Aznarán <gersonec12@gmail.com>
2025-07-06 19:52:36 -0500

    fix(cors): habilitar CORS en la configuración de la aplicación

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   app.useGlobalPipes(
     new ValidationPipe({
       forbidNonWhitelisted: false,
